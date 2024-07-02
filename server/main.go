86c482251765d579c35dd40325b1341709555024
fahriddin <fahriddinrahimberdiyev@gmail.com>
2024-07-02 13:06:35 +0500

    Server FULL Code

--- server/main.go
+++ server/main.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"mymode/config"
+	pb "mymode/generated"
 	"mymode/service"
 	"mymode/storage"
 	"mymode/storage/postgres"
 	"net"
+
 	"google.golang.org/grpc"
-	pb "mymode/generated"
 )
 
-
-
-func main(){
-	db,err := storage.Connect()
-	if err != nil{
+func main() {
+	db, err := storage.Connect()
+	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
 	config := config.Load()
-	listener,err := net.Listen("tcp",":"+config.URL_PORT)
+	listener, err := net.Listen("tcp", ":"+config.URL_PORT)
 
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 
@@ -31,5 +30,7 @@ func main(){
 	s := service.NewHabitTracker(*postgres.NewHabitTrackerRepo(db))
 	server := grpc.NewServer()
 	pb.RegisterHabitTrackerServer(server, s)
-	
+	if err = server.Serve(listener); err != nil {
+		panic(err)
+	}
 }
